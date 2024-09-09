acd0050ca762db3af3c89e4a42287938e6f8553b
Sandra Walgenbach <S.s.s.w.3005@gmail.com>
2024-09-09 16:09:38 +0200

    first commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Sandra Walgenbach on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
